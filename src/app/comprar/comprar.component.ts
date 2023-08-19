06cecafe1fa658c23e32794f9726dbec945fb5db
Ramón Moncholí Ros <ramon.moncholi08@gmail.com>
2023-08-19 08:03:27 +0200

    cambios

--- src/app/comprar/comprar.component.ts
+++ src/app/comprar/comprar.component.ts
@@ -7,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComprarComponent implements OnInit {
 
-  constructor() { }
+  nombre!: string;
+  apellidos!: string;
+  email!: string;
+  dir_env!: string;
+  tlf!: number;
+  message!: string;
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
    
+  }
+
+  comprobar(){
+     if (!this.nombre || this.nombre.trim() === '') {
+    alert("Debes poner el nombre");
+  }
   }
 
 
